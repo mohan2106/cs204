09eb4b8b02ceb8f1604b780e9e5e44a44bd04ec0
Mohan Kumar <mohanjnv1@gmail.com>
2019-11-11 14:55:57 +0530

    Update treaps.cpp

--- labAssignments/treaps.cpp
+++ labAssignments/treaps.cpp
@@ -11,30 +11,23 @@ struct TreapNode
 TreapNode *rightRotate(TreapNode *y) 
 { 
     TreapNode *x = y->left,  *T2 = x->right; 
-  
-    // Perform rotation 
+   
     x->right = y; 
     y->left = T2; 
   
-    // Return new root 
     return x; 
 } 
-  
-// A utility function to left rotate subtree rooted with x 
-// See the diagram given above. 
+ 
 TreapNode *leftRotate(TreapNode *x) 
 { 
     TreapNode *y = x->right, *T2 = y->left; 
-  
-    // Perform rotation 
+ 
     y->left = x; 
     x->right = T2; 
-  
-    // Return new root 
+
     return y; 
 } 
-  
-/* Utility function to add a new key */
+ 
 TreapNode* newNode(int key) 
 { 
     TreapNode* temp = new TreapNode; 
@@ -43,52 +36,41 @@ TreapNode* newNode(int key)
     temp->left = temp->right = NULL; 
     return temp; 
 } 
-  
-// C function to search a given key in a given BST 
+
 TreapNode* search(TreapNode* root, int key) 
 { 
-    // Base Cases: root is null or key is present at root 
+    
     if (root == NULL || root->key == key) 
        return root; 
   
-    // Key is greater than root's key 
     if (root->key < key) 
        return search(root->right, key); 
   
-    // Key is smaller than root's key 
     return search(root->left, key); 
 } 
   
-/* Recursive implementation of insertion in Treap */
 TreapNode* insert(TreapNode* root, int key) 
-{ 
-    // If root is NULL, create a new node and return it 
+{  
     if (!root) 
         return newNode(key); 
   
-    // If key is smaller than root 
     if (key <= root->key) 
     { 
-        // Insert in left subtree 
         root->left = insert(root->left, key); 
   
-        // Fix Heap property if it is violated 
         if (root->left->priority > root->priority) 
             root = rightRotate(root); 
     } 
-    else  // If key is greater 
+    else  
     { 
-        // Insert in right subtree 
         root->right = insert(root->right, key); 
   
-        // Fix Heap property if it is violated 
         if (root->right->priority > root->priority) 
             root = leftRotate(root); 
     } 
     return root; 
 } 
   
-/* Recursive implementation of Delete() */
 TreapNode* deleteNode(TreapNode* root, int key) 
 { 
     if (root == NULL) 
@@ -99,25 +81,22 @@ TreapNode* deleteNode(TreapNode* root, int key)
     else if (key > root->key) 
         root->right = deleteNode(root->right, key); 
   
-    // IF KEY IS AT ROOT 
-  
-    // If left is NULL 
     else if (root->left == NULL) 
     { 
         TreapNode *temp = root->right; 
         delete(root); 
-        root = temp;  // Make right child as root 
+        root = temp;  
     } 
   
-    // If Right is NULL 
+    
     else if (root->right == NULL) 
     { 
         TreapNode *temp = root->left; 
         delete(root); 
-        root = temp;  // Make left child as root 
+        root = temp;  
     } 
   
-    // If ksy is at root and both left and right are not NULL 
+    
     else if (root->left->priority < root->right->priority) 
     { 
         root = leftRotate(root); 
